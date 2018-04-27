cfafb3ae2d3501244560c46ef6decb58d18602c1
eddy17c17c <eddy17c17c@gmail.com>
2018-04-27 16:42:43 +0800

    Version1

---
+++ app/src/main/java/com/example/eddy/onemanband/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.eddy.onemanband;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton guitarImageBtn=(ImageButton)findViewById(R.id.guitarImageBtn);
+        guitarImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToGuitarActivity=new Intent(getApplicationContext(),GuitarActivity.class);
+                startActivity(goToGuitarActivity);
+            }
+        });
+
+    }
+}
