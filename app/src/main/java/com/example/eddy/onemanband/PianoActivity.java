fb1ebe5979bbe39c3f53ce5c1317e46792a380f6
eddy17c17c <eddy17c17c@gmail.com>
2018-05-03 13:41:02 +0800

    Piano button

--- app/src/main/java/com/example/eddy/onemanband/PianoActivity.java
+++ app/src/main/java/com/example/eddy/onemanband/PianoActivity.java
@@ -1,13 +1,26 @@
 package com.example.eddy.onemanband;
 
+import android.media.Image;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PianoActivity extends AppCompatActivity {
+
+    MediaPlayer Do;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piano);
+
+        Do=MediaPlayer.create(this,R.raw.ring);
+
+    }
+
+    public void playDo(View view){
+        Do.start();
     }
 }
--- app/src/main/java/com/example/eddy/onemanband/RingActivity.java
+++ app/src/main/java/com/example/eddy/onemanband/RingActivity.java
@@ -1,11 +1,13 @@
 package com.example.eddy.onemanband;
 
 import android.content.Context;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class RingActivity extends NavActivity {
     MediaPlayer ring;
@@ -19,8 +21,8 @@ public class RingActivity extends NavActivity {
 
         setContentView(R.layout.activity_ring);
 
-
         ring=MediaPlayer.create(this,R.raw.ring);
+
     }
 
     public void playRing(View view){
